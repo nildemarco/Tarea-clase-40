b5b17c62b9c3171f715ebd9e45daeaa68ec2c270
nildemarco <nildemarco@gmail.com>
2019-11-08 22:18:44 -0300

    ejercicios hasta el 8

---
+++ algoritmia.js
@@ -0,0 +1,36 @@
+//Mostrar en consola la suma parcial de todos los números que van desde 0 hasta 1000 (debe mostrar 500500)
+
+let acumuladora = 0
+
+for (i = 0; i <= 1000; i++) {
+   console.log(` ${acumuladora = i + acumuladora}`)   
+}
+
+//Mostrar en consola la suma parcial de todos los números impares que van desde 0 hasta 1000 (debe mostrar 250000)
+
+let acumuladora2 = 0
+
+for (i = 0; i < 1000; i++) {
+    if (i % 2 != 0)  {
+        console.log(`${acumuladora2 = i + acumuladora2}`);
+    }
+} 
+
+//Mostrar en consola el siguiente dibujo
+
+let acumuladora3 = "*"
+console.log(acumuladora3)
+for (i = 0; i < 12; i++) {
+    console.log(`${acumuladora3 = acumuladora3 + "*"}`)
+
+}
+
+// Ejercicio 8
+
+let acumuladora4 = "*"
+console.log(acumuladora4)
+for (i = 0; i < 6; i++) {
+    
+    console.log(`${acumuladora4 = acumuladora4 + "**"}`)
+   
+}
